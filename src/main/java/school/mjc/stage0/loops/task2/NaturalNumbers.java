eb6c3b6024fe6a5fc1e180ec07339f2c9b14583a
NANO <File changed>
2022-07-29 02:07:06 +0600

    4 submission

--- src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.loops.task2;
 public class NaturalNumbers {
     public void naturalNumbersPrinter(int lastPrinted) {
         int i = 0;
-        while (i <= lastPrinted) {
+        while (i < lastPrinted) {
             System.out.println(i + " ");
             i++;
         }
