0669873cb26ecb2a3a85e2e3987b5db32896ea5f
NANO <File changed>
2022-07-29 23:56:25 +0600

    8 submission

--- src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
@@ -2,10 +2,10 @@ package school.mjc.stage0.loops.task2;
 
 public class NaturalNumbers {
     public void naturalNumbersPrinter(int lastPrinted) {
-        int num = 0;
-        while (num <= lastPrinted) {
-            System.out.println(num);
-            num++;
+        int number = 0;
+        while (number <= lastPrinted) {
+            System.out.println(number);
+            number++;
         }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -4,14 +4,12 @@ public class PowerOfTwo {
     public void printPower(int power) {
         if (power < 0) {
             System.out.println("too much power");
-
         } else {
             int i = 0;
             while (i <= power) {
                 System.out.println((int) Math.pow(2, i));
                 i++;
             }
-
         }
     }
 }
