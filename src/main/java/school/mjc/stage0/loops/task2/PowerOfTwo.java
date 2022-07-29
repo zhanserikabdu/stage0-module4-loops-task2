ea831679efa52a5c40aae15235461c14c8f26eab
NANO <File changed>
2022-07-29 23:28:32 +0600

    7 submission

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -4,13 +4,14 @@ public class PowerOfTwo {
     public void printPower(int power) {
         if (power < 0) {
             System.out.println("too much power");
-            return;
-        }
-        int i =0;
-        while (i <= power) {
-            System.out.println((int)Math.pow(2,i));
-            i++;
-        }
 
+        } else {
+            int i = 0;
+            while (i <= power) {
+                System.out.println((int) Math.pow(2, i));
+                i++;
+            }
+
+        }
     }
 }
